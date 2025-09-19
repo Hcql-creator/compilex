65cb9112df3fef8218f455714ec1bf46408d8824
Mendex <mendex224252@gmail.com>
2025-09-19 10:07:06 +0200

    mettre bot en ligne

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
-const { Client } = require("discord.js");
+import {
+  Client,
+  Events,
+  GatewayIntentBits,
+  SlashCommandBuilder,
+} from "discord.js";
+import { config } from "dotenv";
+config();
+
 const token = process.env.BOT_TOKEN_KEY;
 
-const client = new Client();
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, (readyCLient) => {
+  console.log(`Logged in as ${readyCLient.user.tag}`);
+});
+
+client.login(token);
+
+new SlashCommandBuilder().setName("ping").setDescription("Repkies with Pong!");
