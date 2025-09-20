a0b633308b024f0824f009d2c1cdda5f48716674
Rafaël <hocquel.rafael@gmail.com>
2025-09-20 16:56:54 +0200

    aa

--- src/handlers/eventHandler.js
+++ src/handlers/eventHandler.js
@@ -1,6 +1,6 @@
 import { join } from "path";
 import getAllFiles from "../utils/getAllFiles.js";
 export default (client) => {
-  const eventFolders = getAllFiles(join(__dirname, "..", "events"), true);
-  console.log(eventFolders);
+  // const eventFolders = getAllFiles(join(__dirname, "..", "events"), true);
+  // console.log(eventFolders);
 };
