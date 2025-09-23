8b6bfa0aafbf1307d539b327b3d308b3d994d4fa
Rafaël <hocquel.rafael@gmail.com>
2025-09-23 13:46:34 +0200

    mute & updatedMutedRoles

---
+++ src/events/channelCreate/updateMutedRoles.js
@@ -0,0 +1,41 @@
+module.exports = async (client, channel) => {
+  const roleCollection = await channel.guild.roles.fetch();
+  const roles = Array.from(roleCollection.values());
+  const mutedRoles = roles.filter((role) => role.name === "Muted");
+
+  // Si plus de 1 role
+  if (mutedRoles.length > 1)
+    return channel.send(
+      "🔢 Il existe plusieurs roles @Muted, merci d'en supprimer jusqu'a en laisser un seule et unique."
+    );
+
+  // Si le role Muted n'existe pas
+  let mutedRole;
+  if (mutedRoles.length === 0) {
+    console.log("mutedRoles", mutedRoles.length);
+    mutedRole = await channel.guild.roles.create({
+      name: "Muted",
+      colors: 0x808080,
+      // Afficher séparément des autres
+      hoist: true,
+      mentionable: false,
+      permissions: [],
+    });
+  } else {
+    mutedRole = mutedRoles[0];
+  }
+
+  let response;
+  try {
+    await channel.permissionOverwrites.edit(mutedRole, {
+      SendMessages: false,
+      ViewChannel: true,
+    });
+  } catch (error) {
+    response = "Erreur lors de la modification des permissioons";
+  }
+
+  if (response) {
+    channel.send(response);
+  }
+};
