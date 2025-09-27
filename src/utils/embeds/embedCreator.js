c570c939d5cf482caf05182291e7e2e42e8e1f54
Rafaël <hocquel.rafael@gmail.com>
2025-09-27 11:52:57 +0200

    embed functions

---
+++ src/utils/embeds/embedCreator.js
@@ -0,0 +1,39 @@
+const { EmbedBuilder, ThreadAutoArchiveDuration } = require("discord.js");
+
+module.exports = (
+  interaction,
+  color,
+  title,
+  description,
+  thumbnail = "",
+  image = "",
+  footer = true,
+  timestamp = true
+) => {
+  const embed = new EmbedBuilder()
+    .setColor(color)
+    .setTitle(title)
+    .setDescription(description);
+  if (footer) {
+    embed.setFooter({
+      text:
+        interaction.member.nickname ??
+        interaction.user.globalName ??
+        interaction.user.tag,
+      iconURL: interaction.user.displayAvatarURL(),
+    });
+  }
+
+  if (thumbnail) {
+    embed.setThumbnail(thumbnail);
+  }
+
+  if (image) {
+    embed.setImage(image);
+  }
+
+  if (timestamp) {
+    embed.setTimestamp();
+  }
+  return embed;
+};
