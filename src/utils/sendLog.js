5649e25ec0e5ef93de474a9da957cc29c773e8cf
Rafaël <hocquel.rafael@gmail.com>
2025-10-12 16:43:26 +0200

    dsd

--- src/utils/sendLog.js
+++ src/utils/sendLog.js
@@ -1,29 +1,6 @@
 const createEmbed = require("../utils/embeds/embedCreator");
 
 // Fonction réutilisable
-module.exports = async (interaction, title, color, embedContent = '') => {
-    const channelID = "1418257636024913951"; 
-    const guild = interaction.guild;
-    let logChannel = guild.channels.cache.get(channelID);
-    let user;
-    if (interaction.user) {
-      user = interaction.user.tag
-    } else {
-      user = interaction.author.tag
-    }
-
-    const commandName = interaction.commandName;
-   
-    //embed
-    const embed = createEmbed(
-      interaction,
-        color,
-        title,
-        `La commande **${commandName}** a été réalisée\n${embedContent}`,
-    );
-    
-    await logChannel.send({ embeds: [embed] });
-    console.log(`✅ Log envoyé : ${user} a utilisé /${commandName}`);
 module.exports = async (interaction, title, color, embedContent = "") => {
   const channelID = "1418257636024913951";
   const guild = interaction.guild;
@@ -51,7 +28,13 @@ module.exports = async (interaction, title, color, embedContent = "") => {
     const channelID = "1418257636024913951";
     const guild = interaction.guild;
     let logChannel = guild.channels.cache.get(channelID);
-    const user = interaction.user.tag;
+    let user;
+    if (interaction.user) {
+      user = interaction.user.tag;
+    } else {
+      user = interaction.author.tag;
+    }
+
     const commandName = interaction.commandName;
 
     //embed
@@ -64,5 +47,23 @@ module.exports = async (interaction, title, color, embedContent = "") => {
 
     await logChannel.send({ embeds: [embed] });
     console.log(`✅ Log envoyé : ${user} a utilisé /${commandName}`);
+    module.exports = async (interaction, title, color, embedContent = "") => {
+      const channelID = "1418257636024913951";
+      const guild = interaction.guild;
+      let logChannel = guild.channels.cache.get(channelID);
+      const user = interaction.user.tag;
+      const commandName = interaction.commandName;
+
+      //embed
+      const embed = createEmbed(
+        interaction,
+        color,
+        title,
+        `La commande **${commandName}** a été réalisée\n${embedContent}`
+      );
+
+      await logChannel.send({ embeds: [embed] });
+      console.log(`✅ Log envoyé : ${user} a utilisé /${commandName}`);
+    };
   };
 };
