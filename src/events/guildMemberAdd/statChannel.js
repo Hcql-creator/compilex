3bdb3c09041fb799a0fc54a6165817a6122d04fa
NholeCoder <maillardgouasnolan38@gmail.com>
2025-10-16 13:35:36 +0200

    amelioration

--- src/events/guildMemberAdd/statChannel.js
+++ src/events/guildMemberAdd/statChannel.js
@@ -1,5 +1,5 @@
 module.exports = async (client, member) => {
-  const CountChannelID = "1426567044232384562";
+  const CountChannelID = "1428344498060263504";
   const channel = client.channels.cache.get(CountChannelID);
   const memberCount = member.guild.memberCount;
   const newName = `Members : ${memberCount}`;
